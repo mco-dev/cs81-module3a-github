dc7e63a0d5dcadf80b77df237828dcbc1cd9cd9f
Marc Overton <mco.dev@gmail.com>
2026-01-17 12:20:19 -0800

    Added oddOrEven() function to journal.

--- journal.js
+++ journal.js
@@ -2,3 +2,8 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+
+// Returns even or odd depending on the number provided.
+function oddOrEven(n) {
+    return (n % 2 === 0) ? "Even" : "Odd";  //Shorthand if then else statment, check if dividing by two has a remainder.
+}
